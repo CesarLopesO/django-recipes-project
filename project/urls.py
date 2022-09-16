38e24448b57a7b71803c0b33a38b1fff8c8970e9
Cesar <cesarlopesg2@gmail.com>
2022-09-16 17:00:23 -0300

    urls switchs

--- project/urls.py
+++ project/urls.py
@@ -16,22 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.shortcuts import render
-from django.urls import path
 from django.http import HttpResponse
+from django.urls import path,include
 
 
-def home(request):
-    return render(request,'home.html')
-
-def sobre(request):
-    return HttpResponse('zecao')
-
-def contato(request):
-    return HttpResponse('zecatatu')
-      
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('',include('recipes.urls')),
+    path('recipes/',include('recipes.urls')),#dominio.com recipes/
 ]
