96b27d623b0f29ae0ea2826203ef55bb506614b9
Cesar <cesarlopesg2@gmail.com>
2022-10-03 15:49:21 -0300

    status

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'recipes/home.html',context ={ 'name':'Luiz Otavio'})
 # Create your views here.
 def contato(request):
     return HttpResponse('zecatatu')
