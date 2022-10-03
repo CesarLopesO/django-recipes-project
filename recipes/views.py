ad8498192337cc203a8e77f9b1cf003e546ff714
Cesar <cesarlopesg2@gmail.com>
2022-10-03 17:06:29 -0300

    ajustes

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 def home(request):
-    return render(request,'recipes/home.html',context ={ 'name':'tilesburguer'})
+    return render(request,'recipes/pages/home.html',context ={ 'name':'ODALMO'})
 # Create your views here.
